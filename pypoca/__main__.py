45fce83de6826e8ef3ebd5527846ee4d5db4e53c
leandcesar <ccleandroc@gmail.com>
2021-11-14 19:27:19 -0300

    Create setup.cfg

--- pypoca/__main__.py
+++ pypoca/__main__.py
@@ -5,7 +5,12 @@ from dislash import InteractionClient
 from pypoca.config import BotConfig
 
 
-if __name__ == "__main__":
+def run() -> None:
+    """Instantiate, configure and run the bot.
+
+    Connect to Discord client (WebSocket and API). Load all cogs. Start the health
+    check server. And finally, run a loop event initialization blocking call.
+    """
     bot = Bot(command_prefix=BotConfig.prefix)
     client = InteractionClient(bot, test_guilds=BotConfig.guilds_ids)
 
@@ -14,3 +19,8 @@ if __name__ == "__main__":
         bot.load_extension(cog)
 
     bot.run(BotConfig.token)
+    client.teardown()
+
+
+if __name__ == "__main__":
+    run()
