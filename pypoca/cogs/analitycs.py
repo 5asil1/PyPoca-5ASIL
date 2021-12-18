64277aa20010f1366cc963da4c1057231330be10
leandcesar <ccleandroc@gmail.com>
2021-12-18 14:25:24 -0300

    Refactor `Dashbot`

--- pypoca/cogs/analitycs.py
+++ pypoca/cogs/analitycs.py
@@ -4,6 +4,7 @@ from discord.ext.commands import Bot, Cog
 from dislash import SlashInteraction
 
 from pypoca import log, utils
+from pypoca.config import Config
 from pypoca.dashbot import Dashbot
 
 __all__ = ("Analytics", "setup")
@@ -12,45 +13,42 @@ __all__ = ("Analytics", "setup")
 class Analytics(Cog):
     """`Analytics` cog track incoming and outgoing messages."""
 
-    def __init__(self, bot: Bot) -> None:
+    def __init__(self, bot: Bot, api_key: str) -> None:
         self.bot = bot
+        self.dashbot = Dashbot(api_key=api_key)
 
     @Cog.listener()
     async def on_message(self, message: Message) -> None:
         """Called when a Message is created and sent."""
-        if message.author.id != self.bot.user.id:
-            return None
-        embed = message.embeds[0]
-        Dashbot.sent(
-            message=embed.title,
-            guild_id=message.guild.id,
-            author_id=message.author.id,
-            author_name=str(message.author),
-            embed=embed.to_dict(),
-        )
+        if message.author.id == self.bot.user.id:
+            embed = message.embeds[0]
+            self.dashbot.sent(
+                message=embed.title,
+                guild_id=message.guild.id,
+                author_id=message.author.id,
+                author_name=str(message.author),
+                embed=embed.to_dict(),
+            )
 
     @Cog.listener()
     async def on_message_edit(self, before: Message, after: Message) -> None:
         """Called when a Message receives an update event."""
-        if after.author.id != self.bot.user.id:
-            return None
-        embed = after.embeds[0]
-        if embed.title == before.embeds[0].title:
-            return None
-        Dashbot.sent(
-            message=embed.title,
-            guild_id=after.guild.id,
-            author_id=after.author.id,
-            author_name=str(after.author),
-            embed=embed.to_dict(),
-        )
+        if after.author.id == self.bot.user.id and after.embeds[0].title != before.embeds[0].title:
+            embed = after.embeds[0]
+            self.dashbot.sent(
+                message=embed.title,
+                guild_id=after.guild.id,
+                author_id=after.author.id,
+                author_name=str(after.author),
+                embed=embed.to_dict(),
+            )
 
     @Cog.listener()
     async def on_slash_command(self, inter: SlashInteraction) -> None:
         """Called when a slash command is invoked."""
         options = utils.slash_data_option_to_str(inter.data.options)
         text = f"/{inter.data.name} {options}"
-        Dashbot.received(
+        self.dashbot.received(
             message=text,
             guild_id=inter.guild.id,
             author_id=inter.author.id,
@@ -61,7 +59,7 @@ class Analytics(Cog):
     async def on_button_click(self, inter: SlashInteraction) -> None:
         """Called when any button is clicked."""
         text = inter.clicked_button.label
-        Dashbot.received(
+        self.dashbot.received(
             message=text,
             guild_id=inter.guild.id,
             author_id=inter.author.id,
@@ -72,7 +70,7 @@ class Analytics(Cog):
     async def on_dropdown(self, inter: SlashInteraction) -> None:
         """Called when any menu is clicked."""
         text = " ".join([option.label for option in inter.select_menu.selected_options])
-        Dashbot.received(
+        self.dashbot.received(
             message=text,
             guild_id=inter.guild.id,
             author_id=inter.author.id,
@@ -82,7 +80,8 @@ class Analytics(Cog):
 
 def setup(bot: Bot) -> None:
     """Setup `Analytics` cog."""
-    if Dashbot.api_key is None:
+    api_key = Config.dashbot.key
+    if api_key is None:
         log.warning("No Dashbot API key configured, couldn't track")
     else:
-        bot.add_cog(Analytics(bot))
+        bot.add_cog(Analytics(bot, api_key))
--- pypoca/dashbot.py
+++ pypoca/dashbot.py
@@ -1,64 +1,44 @@
 # -*- coding: utf-8 -*-
+import aiohttp
 import asyncio
-
-from aiohttp import ClientSession
-
-from pypoca.config import Config
+from dataclasses import dataclass
 
 __all__ = "Dashbot"
 
 
+@dataclass
 class Dashbot:
     """Dashbot provides easy access to bot analytics."""
 
-    api_key = Config.dashbot.key
+    api_key: str
+    url: str = "https://tracker.dashbot.io/track"
+    version: str = "11.1.0-rest"
+    platform: str = "universal"
 
-    @staticmethod
-    async def _request(type: str, *, user_id: str, text: str, **kwargs) -> None:
+    def params(self, type: str) -> dict:
+        """Build query params for HTTP request."""
+        return {"type": type, "v": self.version, "platform": self.platform, "apiKey": self.api_key}
+
+    def json(self, *, text: str, id: str, name: str, locale: str, extra_json: dict = {}) -> dict:
+        """Build JSON payload for HTTP request."""
+        user_json = {"firstName": name, "locale": locale}
+        return {"userId": id, "text": text, "platformJson": extra_json, "platformUserJson": user_json}
+
+    async def _request(self, url: str, *, params: dict, json: dict, **kwargs) -> None:
         """Request to track conversation events on Dashbot."""
-        url = "https://tracker.dashbot.io/track"
-        params = {
-            "v": "11.1.0-rest",
-            "platform": "universal",
-            "type": type,
-            "apiKey": Dashbot.api_key,
-        }
-        data = {
-            "userId": user_id,
-            "text": text,
-            "platformJson": kwargs.get("embed"),
-            "platformUserJson": {
-                "firstName": kwargs.get("user_name"),
-                "locale": kwargs.get("locale"),
-            },
-        }
-        async with ClientSession(raise_for_status=True) as session:
-            async with session.post(url, params=params, json=data) as response:
+        async with aiohttp.ClientSession(raise_for_status=True) as session:
+            async with session.post(url, params=params, json=json) as response:
                 return await response.json()
 
-    @classmethod
-    def received(cls, message: str, guild_id: int, author_id: int, author_name: str) -> None:
+    def received(self, message: str, guild_id: int, author_id: int, author_name: str) -> None:
         """When the bot receives a message."""
-        asyncio.create_task(
-            cls._request(
-                "incoming",
-                text=message,
-                locale=guild_id,
-                user_id=author_id,
-                user_name=author_name,
-            )
-        )
+        params = self.params(type="incoming")
+        json = self.json(text=message, id=author_id, name=author_name, locale=guild_id)
+        asyncio.create_task(self._request(self.url, params=params, json=json))
 
-    @classmethod
-    def sent(cls, message: str, guild_id: int, author_id: int, author_name: str, embed: dict) -> None:
+    def sent(self, message: str, guild_id: int, author_id: int, author_name: str, embed: dict) -> None:
         """When the bot sends a message."""
-        asyncio.create_task(
-            cls._request(
-                "outgoing",
-                text=message,
-                locale=guild_id,
-                user_id=author_id,
-                user_name=author_name,
-                embed={k: v for k, v in embed.items() if v},
-            )
-        )
+        embed = {k: v for k, v in embed.items() if v}  # remove `discord.Embed.Empty` values
+        params = self.params(type="outgoing")
+        json = self.json(text=message, id=author_id, name=author_name, locale=guild_id, extra_json=embed)
+        asyncio.create_task(self._request(self.url, params=params, json=json))
